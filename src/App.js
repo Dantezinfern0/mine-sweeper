3b003b7f90d0e22073c716e504f779a6001b97ec
dantezinfern0 <danteharasz@gmail.com>
2019-05-26 19:52:35 -0400

    not enough committing lately

--- src/App.js
+++ src/App.js
@@ -1,9 +1,59 @@
 import React, { Component } from 'react'
-import HelloWorld from './components/HelloWorld'
+import Squares from './components/Squares'
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    board: [],
+    difficulty: []
+    }
+  
+  componentDidMount = (row, column) => {
+    fetch('https://minesweeper-api.herokuapp.com/games', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ difficulty: 0 })
+    })
+      .then(resp => {
+        return resp.json()
+      })
+      .then(game => {
+        console.log({ game })
+        this.setState({
+          board: game.board
+        })
+        console.log(game.board)
+      })
+  }
+  
   render() {
-    return <HelloWorld />
+    return (
+      <>
+        <main>
+          <header>Time To Play Mine Sweeper</header>
+          <table>
+            <tbody >
+              {this.state.board.map((row, i) => {
+                return (
+                  <tr Key={i}>
+                    {row.map((column, j) => {
+                      return (
+                        <td key={j} row={i} col={j} value={column
+                        } class="td-box-class" onClick="" onContextMenu="">
+                          {this.state.board[i][j]}
+                        </td>
+                      )
+                    })}
+                  </tr>
+                )
+              })}
+            </tbody>
+          </table>
+        </main>
+      </>
+    )
   }
 }
 
---
+++ src/components/Squares.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+class Squares extends Component {
+  render() {
+    return <>
+    <button class="game-button"></button>
+    </>
+  }
+}
+
+export default Squares
